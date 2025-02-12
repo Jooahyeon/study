2636afdaa2696a17c7f84dfa0280695569284bd5
Jooahyeon <dkgus0496@naver.com>
2025-02-12 20:58:17 +0900

    0212수업

---
+++ 00.me/04.Algo/0212Algo/src/main/java/bj2857/BJ2857.java
@@ -0,0 +1,34 @@
+package bj2857;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BJ2857 {
+    public static void main(String[] args) throws IOException {
+
+        /* 설명.첩보원명은 FBI */
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+
+        List<String> aswerList =
+
+
+        // 한줄씩 입력 및
+        String str = br.readLine();
+
+        //FBI가 어딨는지 찾기...?(줄단위로)
+        for (int i = 0; i <str.length(); i++) {
+            if ( str =
+
+            )
+
+
+                //String.contains        }
+
+
+
+
+    }
+}
